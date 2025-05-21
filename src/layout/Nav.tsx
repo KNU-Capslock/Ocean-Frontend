a529bcee80c1f616acff5a760c08cfe1a48c89bc
MOJAN <sjs732@naver.com>
2025-05-21 15:05:07 +0900

    Feat: 카메라 이동 버튼 추가

--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -4,15 +4,22 @@ import {
   ClosetIcon,
   HomeIcon,
 } from "@components/Icons";
+import { useNavigate } from "react-router-dom";
 
 const Nav = () => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="mt-16" />
       <nav className="fixed bottom-0 flex items-center justify-between w-full max-w-[512px] bg-white py-3 px-10">
         <HomeIcon className="w-6 h-6" />
         <ClosetIcon className="w-6 h-6" />
-        <div className="h-10 p-2 rounded-lg bg-neutral-200">
+        <div
+          className="h-10 p-2 rounded-lg bg-neutral-200"
+          onClick={() => {
+            navigate("/camera");
+          }}
+        >
           <CameraIcon className="w-6 h-6" />
         </div>
         <CalenderIcon className="w-6 h-6" />
