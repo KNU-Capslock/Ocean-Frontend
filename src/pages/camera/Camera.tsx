8f656226ad2ef6455bc0558175c46fea277401e8
MOJAN <sjs732@naver.com>
2025-05-28 22:34:30 +0900

    Refactor: Camera 리팩토링

--- src/pages/camera/Camera.tsx
+++ src/pages/camera/Camera.tsx
@@ -1,10 +1,67 @@
-import CameraCapture from "@components/CameraCapture";
+import { useRef, useEffect } from "react";
+import useCamera from "@/hooks/useCamera";
+import { useNavigate } from "react-router-dom";
 
 const Camera = () => {
+  const { videoRef, start, stop } = useCamera();
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const navigate = useNavigate();
+  const takePhoto = () => {
+    if (!videoRef.current || !canvasRef.current) return;
+    const { videoWidth: w, videoHeight: h } = videoRef.current;
+    canvasRef.current.width = w;
+    canvasRef.current.height = h;
+    const ctx = canvasRef.current.getContext("2d")!;
+    ctx.drawImage(videoRef.current, 0, 0, w, h);
+    canvasRef.current.toBlob((blob) => {
+      if (!blob) return;
+      const url = URL.createObjectURL(blob);
+      console.log(url);
+      navigate("/camera/result?url=" + url);
+    }, "image/jpeg");
+    stop();
+  };
+
+  useEffect(() => {
+    start();
+    return () => {
+      stop();
+    };
+  }, []);
+
   return (
-    <>
-      <CameraCapture />
-    </>
+    <div className="flex flex-col justify-center flex-grow">
+      <div
+        style={{ display: videoRef.current ? "flex" : "none" }}
+        className="text-center"
+      >
+        <video
+          ref={videoRef}
+          autoPlay
+          playsInline
+          style={{
+            height: "calc(100vh - 7.5rem)",
+            width: "100%",
+            aspectRatio: "9 / 16",
+            objectFit: "cover",
+            background: "black",
+            flexGrow: "1",
+          }}
+        />
+        <button
+          onClick={() => {
+            console.log("adsf");
+            takePhoto();
+          }}
+          style={{
+            transform: "translateX(-50%) translateY(-7em)",
+          }}
+          className="absolute bottom-0 z-10 w-16 h-16 bg-white rounded-full left-1/2"
+        />
+      </div>
+      <canvas ref={canvasRef} style={{ display: "none" }} />
+    </div>
   );
 };
 
