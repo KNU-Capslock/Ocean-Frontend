e71f752672b6876fec0ddec93c623d98c57af94f
MOJAN <sjs732@naver.com>
2025-05-28 23:14:55 +0900

    Feat: Header 텍스트 추가

---
+++ src/constants/HeaderText.ts
@@ -0,0 +1,4 @@
+export const HeaderText: { [key: string]: string } = {
+  "/camera": "사진 촬영",
+  "/camera/result": "사진 등록하기",
+};
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,14 +1,36 @@
-import { NotiIcon } from "@components/Icons";
+import { HeaderText } from "@/constants/HeaderText";
+import { LeftIcon, NotiIcon } from "@components/Icons";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const [text, setText] = useState<String | null>();
+  useEffect(() => {
+    setText(HeaderText[pathname] ?? null);
+  }, [pathname]);
   return (
     <>
-      <header className="fixed top-0 w-full max-w-[512px] flex justify-between px-4 py-3 bg-white">
-        <div className="w-6" />
-        <h1 className="text-xl font-extrabold">OCEAN</h1>
+      <header className="fixed top-0 w-full max-w-[512px] flex items-center justify-between px-4 py-3 border-b border-neutral-200 bg-white">
+        {text ? (
+          <LeftIcon
+            className="w-6 h-6 cursor-pointer"
+            onClick={() => {
+              navigate(-1);
+            }}
+          />
+        ) : (
+          <div className="w-6" />
+        )}
+        {text ? (
+          <h1 className="text-lg font-bold">{text}</h1>
+        ) : (
+          <h1 className="text-xl font-extrabold">OCEAN</h1>
+        )}
         <NotiIcon className="w-6 h-6 bg-neutral-600" />
       </header>
-      <div className="mb-14" />
+      <div className="mb-12" />
     </>
   );
 };
