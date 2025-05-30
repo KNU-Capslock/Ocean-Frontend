61346c276cd5d6db558a96c38fe7bfd9088c6aa2
MOJAN <sjs732@naver.com>
2025-05-31 04:32:33 +0900

    Feat: Clothes 페이지 추가

---
+++ src/pages/clothes/Clothes.tsx
@@ -0,0 +1,11 @@
+import ClothesContainer from "@components/clothes/ClothesContainer";
+
+const Clothes = () => {
+  return (
+    <>
+      <ClothesContainer clothImg={""} clothType={""} />
+    </>
+  );
+};
+
+export default Clothes;
