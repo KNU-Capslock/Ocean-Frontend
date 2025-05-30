7f4a81315a54bcbdc3b8e4543e87dcab0dc260f5
MOJAN <sjs732@naver.com>
2025-05-31 04:25:42 +0900

    Feat: 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Main from "@pages/main/Main";
 import Camera from "@pages/camera/Camera";
 import CameraResult from "@pages/camera/CameraResult";
+import Clothes from "@pages/clothes/Clothes";
+import Onboarding from "@pages/Onboarding/Onboarding";
+import Login from "@pages/login/Login";
+import Register from "@pages/register/Register";
+import Post from "@pages/post/Post";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -14,6 +19,11 @@ const App = () => {
         <Layout>
           <Routes>
             <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/onboard" element={<Onboarding />} />
+            <Route path="/clothes" element={<Clothes />} />
+            <Route path="/post" element={<Post />} />
             <Route path="/camera" element={<Camera />} />
             <Route path="/camera/result" element={<CameraResult />} />
           </Routes>
