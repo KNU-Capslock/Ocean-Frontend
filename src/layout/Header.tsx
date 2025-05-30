a5162c0ed2143660496f18124a3f2c12d0a31274
MOJAN <sjs732@naver.com>
2025-05-31 04:28:16 +0900

    Feat: 특정 페이지에서는 헤더, Nav 숨김

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,31 +7,39 @@ const Header = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const [text, setText] = useState<String | null>();
+  const [headerVisible, setHeaderVisible] = useState(true);
+
   useEffect(() => {
     setText(HeaderText[pathname] ?? null);
+    setHeaderVisible(
+      pathname != "/onboard" && pathname != "/login" && pathname != "/register"
+    );
   }, [pathname]);
+
   return (
-    <>
-      <header className="fixed top-0 w-full max-w-[512px] flex items-center justify-between px-4 py-3 border-b border-neutral-200 bg-white">
-        {text ? (
-          <LeftIcon
-            className="w-6 h-6 cursor-pointer"
-            onClick={() => {
-              navigate(-1);
-            }}
-          />
-        ) : (
-          <div className="w-6" />
-        )}
-        {text ? (
-          <h1 className="text-lg font-bold">{text}</h1>
-        ) : (
-          <h1 className="text-xl font-extrabold">OCEAN</h1>
-        )}
-        <NotiIcon className="w-6 h-6 bg-neutral-600" />
-      </header>
-      <div className="mb-12" />
-    </>
+    headerVisible && (
+      <>
+        <header className="fixed top-0 w-full z-[1] max-w-[512px] flex items-center justify-between px-4 py-3 border-b border-neutral-200 bg-white">
+          {text ? (
+            <LeftIcon
+              className="w-6 h-6 cursor-pointer"
+              onClick={() => {
+                navigate(-1);
+              }}
+            />
+          ) : (
+            <div className="w-6" />
+          )}
+          {text ? (
+            <h1 className="text-lg font-bold">{text}</h1>
+          ) : (
+            <h1 className="text-xl font-extrabold">OCEAN</h1>
+          )}
+          <NotiIcon className="w-6 h-6 bg-neutral-600" />
+        </header>
+        <div className="mb-12" />
+      </>
+    )
   );
 };
 
--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -4,35 +4,51 @@ import {
   ClosetIcon,
   HomeIcon,
 } from "@components/Icons";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Nav = () => {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
+  const [navVisible, setNavVisible] = useState(true);
+
+  useEffect(() => {
+    setNavVisible(
+      pathname != "/onboard" && pathname != "/login" && pathname != "/register"
+    );
+  }, [pathname]);
   return (
-    <>
-      <div className="mt-16" />
-      <nav className="fixed bottom-0 flex items-center justify-between w-full max-w-[512px] bg-white py-3 px-10">
-        <HomeIcon
-          onClick={() => {
-            navigate("/");
-          }}
-          className="w-6 h-6 cursor-pointer"
-        />
-        <ClosetIcon className="w-6 h-6" />
-        <div
-          className="h-10 p-2 rounded-lg cursor-pointer bg-neutral-200"
-          onClick={() => {
-            navigate("/camera");
-          }}
-        >
-          <CameraIcon className="w-6 h-6" />
-        </div>
-        <CalenderIcon className="w-6 h-6" />
-        <div className="w-6 h-6 overflow-hidden border rounded-full border-neutral-300">
-          <img src="https://picsum.photos/256/256" />
-        </div>
-      </nav>
-    </>
+    navVisible && (
+      <>
+        <div className="mt-16" />
+        <nav className="fixed bottom-0 flex items-center justify-between w-full max-w-[512px] bg-white py-3 px-10">
+          <HomeIcon
+            onClick={() => {
+              navigate("/");
+            }}
+            className="w-6 h-6 cursor-pointer"
+          />
+          <ClosetIcon
+            onClick={() => {
+              navigate("/clothes");
+            }}
+            className="w-6 h-6 cursor-pointer"
+          />
+          <div
+            className="h-10 p-2 rounded-lg cursor-pointer bg-neutral-200"
+            onClick={() => {
+              navigate("/camera");
+            }}
+          >
+            <CameraIcon className="w-6 h-6" />
+          </div>
+          <CalenderIcon className="w-6 h-6" />
+          <div className="w-6 h-6 overflow-hidden border rounded-full border-neutral-300">
+            <img src="https://picsum.photos/256/256" />
+          </div>
+        </nav>
+      </>
+    )
   );
 };
 
